c18ae7a9ebb78a9747f9e5e84d333648a1951cdc
HhCruz21 <haizonhelet@gmail.com>
2026-01-18 17:49:20 +0100

    Completed TCP server skeleton, sample batching, drop oldest data from buffer

--- src/headers.h
+++ src/headers.h
@@ -29,4 +29,12 @@ typedef struct
     int32_t value;
 } __attribute__((packed)) sample_t; // __attribute__((packed)) sample_t, compliers may insert padding, affects performance.
 
+typedef struct {
+    int64_t min_jitter_us;
+    int64_t max_jitter_us;
+    int64_t sum_jitter_us;
+    uint64_t samples;
+    uint64_t missed_deadlines;
+} __attribute__((packed)) jitter_stats_t;
+
 #endif
--- src/threads.c
+++ src/threads.c
@@ -9,6 +9,21 @@
 #include <poll.h>
 #include <sched.h>
 
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <string.h>
+
+#define PORT_NUMBER 5000
+#define MAX_BATCH_SAMPLES 64            //can be increased, it is purposely set low to allow margin
+#define MAX_PAYLOAD_BYTES (MAX_BATCH_SAMPLES * sizeof(sample_t))
+
+static inline int64_t timeUs(const struct timespec *t)
+{
+    return (int64_t)t->tv_sec * 1000000LL +
+           (int64_t)t->tv_nsec / 1000LL;
+}
+
 static void addUs(struct timespec *t, long us)
 {
     t->tv_nsec += us * 1000;
@@ -19,21 +34,25 @@ static void addUs(struct timespec *t, long us)
     }
 }
 
-static void setRealTime(int priority){
+static void setRealTime(int priority)
+{
     struct sched_param sp;
     sp.sched_priority = priority;
-    
-    if(sched_setscheduler(0, SCHED_FIFO, &sp) != 0){
+
+    if (sched_setscheduler(0, SCHED_FIFO, &sp) != 0)
+    {
         perror("sched_setscheduler");
     }
 }
 
-static void setCpuAffinity(int cpu){
+static void setCpuAffinity(int cpu)
+{
     cpu_set_t cpuset;
     CPU_ZERO(&cpuset);
     CPU_SET(cpu, &cpuset);
 
-    if(sched_setaffinity(0, sizeof(cpuset), &cpuset) != 0){
+    if (sched_setaffinity(0, sizeof(cpuset), &cpuset) != 0)
+    {
         perror("sched_setaffinity");
     }
 }
@@ -43,9 +62,17 @@ void *snsrThrdFunc(void *arg)
     setCpuAffinity(0);
     setRealTime(80);
 
+    static jitter_stats_t jitterStats = {
+    .min_jitter_us = 1000000,
+    .max_jitter_us = -1000000,
+    .sum_jitter_us = 0,                         //will overflow if it runs for a long time
+    .samples = 0,
+    .missed_deadlines = 0};
+
     ring_buffer_t *rb = (ring_buffer_t *)arg;
 
     struct timespec next;
+    struct timespec now;
     clock_gettime(CLOCK_MONOTONIC, &next);
 
     uint32_t tick = 0;
@@ -85,6 +112,25 @@ void *snsrThrdFunc(void *arg)
     while (1)
     {
         clock_nanosleep(CLOCK_MONOTONIC, TIMER_ABSTIME, &next, NULL);
+        clock_gettime(CLOCK_MONOTONIC, &now);
+
+        int64_t expectedUs = timeUs(&next);
+        int64_t actualUs = timeUs(&now);
+        int64_t jitterUs = actualUs - expectedUs;
+
+        /* Update stats*/
+        if (jitterUs < jitterStats.min_jitter_us)
+            jitterStats.min_jitter_us = jitterUs;
+
+        if (jitterUs > jitterStats.max_jitter_us)
+            jitterStats.max_jitter_us = jitterUs;
+
+        jitterStats.sum_jitter_us += jitterUs;
+        jitterStats.samples++;
+
+        /* Missed deadline detection*/
+        if (jitterUs > (int64_t)BASE_TICK_US)
+            jitterStats.missed_deadlines++;
 
         /* -------- PT100: 10 Hz (every 100 ms) -------- */
         if ((tick % 1000U) == 0U)
@@ -167,6 +213,17 @@ void *snsrThrdFunc(void *arg)
                 }
             }
         }
+
+        if ((jitterStats.samples % 10000ULL) == 0ULL)
+        {
+            int64_t avg = jitterStats.sum_jitter_us / (int64_t)jitterStats.samples;
+
+            printf("[JITTER] min=%ld us, max=%ld us, avg=%ld us, missed=%lu\n", 
+                jitterStats.min_jitter_us, 
+                jitterStats.max_jitter_us, 
+                avg, 
+                jitterStats.missed_deadlines);
+        }
         tick++;
         addUs(&next, BASE_TICK_US);
     }
@@ -177,19 +234,88 @@ void *ntwrkThrdFunc(void *arg)
 {
     setCpuAffinity(1);
     setRealTime(20);
-    
+
     ring_buffer_t *rb = (ring_buffer_t *)arg;
     sample_t s;
 
+    int serverFd = socket(AF_INET, SOCK_STREAM, 0);
+    if(serverFd < 0){
+        perror("socket");
+        return NULL;
+    }
+
+    int opt = 1;
+    setsockopt(serverFd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt));
+
+    struct sockaddr_in addr = {
+        .sin_family = AF_INET,
+        .sin_port = htons(PORT_NUMBER),
+        .sin_addr.s_addr = INADDR_ANY
+    };
+
+    if(bind(serverFd, (struct sockaddr*)&addr, sizeof(addr)) < 0){
+        perror("bind");
+        return NULL;
+    }
+
+    if(listen(serverFd, 1) < 0){
+        perror("listen");
+        return NULL;
+    }
+
+    printf("TCP server listening on port %d\n", PORT_NUMBER);
+
+    int clientFd = accept(serverFd, NULL, NULL);
+    if(clientFd < 0){
+        perror("accept");
+        return NULL;
+    }
+
+    printf("Client connected...\n");
+
+    uint8_t txBuffer[2 + MAX_PAYLOAD_BYTES];
+    sample_t batch[MAX_BATCH_SAMPLES];
+
     while (1)
     {
-        if (ringBufferPop(rb, &s) == 0)
-        {
+        uint32_t count = ringBufferCount(rb);
+
+        /* Drop oldest samples if backlog grows */
+        if(count > MAX_BATCH_SAMPLES){
+            uint32_t drop = count - MAX_BATCH_SAMPLES;
+            while(drop--){
+                ringBufferPop(rb, &s);
+            }
         }
-        else
-        {
+
+        size_t n = 0;
+        while(n < MAX_BATCH_SAMPLES){
+            if(ringBufferPop(rb, &batch[n]) == 0)
+                n++;
+            else
+                break;
+        }
+
+        if(n == 0){
             usleep(1000);
+            continue;
+        }
+
+        uint16_t payloadLen = n * sizeof(sample_t);
+        txBuffer[0] = (payloadLen >> 8) & 0xFF;
+        txBuffer[1] = payloadLen & 0xFF;
+
+        memcpy(&txBuffer[2], batch, payloadLen);
+
+        ssize_t sent = send(clientFd, txBuffer, payloadLen + 2, 0);
+        if(sent <= 0){
+            perror("send");
+            break;
         }
     }
+
+    /* Terminate client socket and server socket*/
+    close(clientFd);
+    close(serverFd);
     return NULL;
 }
